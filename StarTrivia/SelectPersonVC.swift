d83612b47f5d1e18a43f0265f8adb826aec349ea
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-06-09 22:19:35 +0900

    メインbio作成

--- StarTrivia/SelectPersonVC.swift
+++ StarTrivia/SelectPersonVC.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class SelectPersonVC: UIViewController {
 
+    @IBOutlet weak var stackBg: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stackBg.layer.cornerRadius = 10
     }
 
 
