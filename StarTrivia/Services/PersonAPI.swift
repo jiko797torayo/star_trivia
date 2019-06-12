78c8cf3283ac3f765162620ed3c46ab199b8b594
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-06-12 22:48:46 +0900

    Codableを使って実装しやすいデータ型に変換

--- StarTrivia/Services/PersonAPI.swift
+++ StarTrivia/Services/PersonAPI.swift
@@ -12,28 +12,51 @@ import SwiftyJSON
 
 class PersonApi {
     
-    // Web Request with Alamofire and SwiftyJson
-    func getRandomPersonAlamo(id: Int, completion: @escaping PersonResponseCompletion) {
-        
-        guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
-        Alamofire.request(url).responseJSON { (response) in
-            if let error = response.result.error {
-                debugPrint(error.localizedDescription)
-                completion(nil)
-                return
-            }
-            
-            guard let data = response.data else { return completion(nil)}
-            do {
-                let json = try JSON(data: data)
-                let person = self.parsePersonSwifty(json: json)
-                completion(person)
-            } catch {
-                debugPrint(error.localizedDescription)
-                completion(nil)
+//     Web Request with Alamofire and Codable
+        func getRandomPersonAlamo(id: Int, completion: @escaping PersonResponseCompletion) {
+    
+            guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
+            Alamofire.request(url).responseJSON { (response) in
+                if let error = response.result.error {
+                    debugPrint(error.localizedDescription)
+                    completion(nil)
+                    return
+                }
+    
+                guard let data = response.data else { return completion(nil)}
+                let jsonDecoder = JSONDecoder()
+                do {
+                    let person = try jsonDecoder.decode(Person.self, from: data)
+                    completion(person)
+                } catch {
+                    debugPrint(error.localizedDescription)
+                    completion(nil)
+                }
             }
         }
-    }
+    
+    // Web Request with Alamofire and SwiftyJson
+//    func getRandomPersonAlamo(id: Int, completion: @escaping PersonResponseCompletion) {
+//
+//        guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
+//        Alamofire.request(url).responseJSON { (response) in
+//            if let error = response.result.error {
+//                debugPrint(error.localizedDescription)
+//                completion(nil)
+//                return
+//            }
+//
+//            guard let data = response.data else { return completion(nil)}
+//            do {
+//                let json = try JSON(data: data)
+//                let person = self.parsePersonSwifty(json: json)
+//                completion(person)
+//            } catch {
+//                debugPrint(error.localizedDescription)
+//                completion(nil)
+//            }
+//        }
+//    }
     
     // Web Request with Alamofire
 //    func getRandomPersonAlamo(id: Int, completion: @escaping PersonResponseCompletion) {
