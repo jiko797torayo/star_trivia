f96d13a65ddded2f476d33ca396edf8e1381d160
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-06-16 22:24:02 +0900

    Vehiclesデータ表示

--- StarTrivia/Controller/VehiclesVC.swift
+++ StarTrivia/Controller/VehiclesVC.swift
@@ -21,10 +21,33 @@ class VehiclesVC: UIViewController, PersonProtocol {
     @IBOutlet weak var nextBtn: UIButton!
     
     var person: Person!
+    let api = VehicleApi()
+    var vehicles = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(person.name)
+        vehicles = person.vehicleUrls
+        guard  let firstVehicle = vehicles.first else { return }
+        getVehicle(url: firstVehicle)
+    }
+    
+    func getVehicle(url: String) {
+        api.getVehicle(url: url) { (vehicle) in
+            if let vehicle = vehicle {
+                self.setupView(vehicle: vehicle)
+            }
+        }
+    }
+    
+    func setupView(vehicle: Vehicle) {
+        nameLbl.text = vehicle.name
+        modelLbl.text = vehicle.model
+        makerLbl.text = vehicle.manufacturer
+        costLbl.text = vehicle.cost
+        lengthLbl.text = vehicle.length
+        speedLbl.text = vehicle.speed
+        crewLbl.text = vehicle.crew
+        pxLbl.text = vehicle.passengers
     }
     @IBAction func previousClicked(_ sender: Any) {
     }
