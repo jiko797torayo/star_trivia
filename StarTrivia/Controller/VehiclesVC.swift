66789ca1fa55208119450270b0456de01d5d9c2d
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-06-16 22:00:21 +0900

    Vehiclesビュー作成

--- StarTrivia/Controller/VehiclesVC.swift
+++ StarTrivia/Controller/VehiclesVC.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class VehiclesVC: UIViewController, PersonProtocol {
-
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var modelLbl: UILabel!
+    @IBOutlet weak var makerLbl: UILabel!
+    @IBOutlet weak var costLbl: UILabel!
+    @IBOutlet weak var lengthLbl: UILabel!
+    @IBOutlet weak var speedLbl: UILabel!
+    @IBOutlet weak var crewLbl: UILabel!
+    @IBOutlet weak var pxLbl: UILabel!
+    @IBOutlet weak var previousBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
     var person: Person!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(person.name)
+    }
+    @IBAction func previousClicked(_ sender: Any) {
+    }
+    @IBAction func nextClicked(_ sender: Any) {
     }
 }
