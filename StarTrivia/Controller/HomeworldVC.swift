70252cdaa35eb7ec7d6cd2554cb31bc9394b36c1
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-06-16 17:02:05 +0900

    4つのボタン押下時に遷移するページのViewControllerを作成

---
+++ StarTrivia/Controller/HomeworldVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeworldVC.swift
+//  StarTrivia
+//
+//  Created by 大石耕司 on 2019/06/16.
+//  Copyright © 2019 大石耕司. All rights reserved.
+//
+
+import UIKit
+
+class HomeworldVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
